9f8df215b67446bb74d8dc1cbde249816d0f9f22
Dwi Candra Permana <52048022+dwichan0905@users.noreply.github.com>
2020-08-26 10:03:35 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,12 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$route['default_controller'] = 'welcome';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
+
 $route['mahasiswa/get_data']['GET'] = 'MahasiswaController/get_mahasiswa';
 $route['mahasiswa/insert_data']['POST'] = 'MahasiswaController/insert_mahasiswa';
 $route['mahasiswa/update_data']['POST'] = 'MahasiswaController/update_mahasiswa';
 $route['mahasiswa/delete_data']['POST'] = 'MahasiswaController/delete_mahasiswa';
-
-
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
